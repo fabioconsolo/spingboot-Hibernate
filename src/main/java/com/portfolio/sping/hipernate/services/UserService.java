be56f6d8eb24a97ed4262e3aa4913094cb6b4b3e
Fabio Matarazzo <fabio.consolo@yahoo.com.br>
2023-04-25 06:30:12 -0300

    Exception handling - delete

--- src/main/java/com/portfolio/sping/hipernate/services/UserService.java
+++ src/main/java/com/portfolio/sping/hipernate/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.portfolio.sping.hipernate.entitites.User;
 import com.portfolio.sping.hipernate.repositories.UserRepository;
+import com.portfolio.sping.hipernate.services.exceptions.DatabaseException;
 import com.portfolio.sping.hipernate.services.exceptions.ResourceNotFoundException;
 
 
@@ -36,9 +39,15 @@ return	repository.save(obj);
 	
 }
 public void delete(Long id) {
-	repository.deleteById(id);
-	
-	
+	try {
+repository.deleteById(id);
+	}	
+	catch(EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id);
+	}
+	catch (DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
+	}
 	
 }
 public User update(Long id, User obj) {
